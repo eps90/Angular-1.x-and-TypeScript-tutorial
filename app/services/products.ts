86221ccec70ae10e285e503b9da71a3bdba84776
Kuba Turek <kubus1990@gmail.com>
2015-12-19 13:11:37 +0100

    Create simple products service

---
+++ app/services/products.ts
@@ -0,0 +1,19 @@
+// app/services/products.ts
+
+/// <reference path="../../typings/tsd.d.ts" />
+
+module MyShop {
+    export class Product {
+        name: string = '';
+        price: number = 0;
+        description: string = '';
+    }
+
+    export class ProductsService {
+        private products: Product[];
+
+        getProducts(): Product[] {
+            return null;
+        }
+    }
+}

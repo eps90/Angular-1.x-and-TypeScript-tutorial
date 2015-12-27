bf4134986c87c42ceaa938c4095c6974a9a68fc3
Kuba Turek <ja@kubaturek.pl>
2015-12-27 16:51:42 +0100

    Adapt tests to ProductsFactory

--- app/services/products.ts
+++ app/services/products.ts
@@ -27,20 +27,15 @@ module MyShop {
     }
 
     export class ProductsService {
-        static $inject = ['$http', '$q'];
+        static $inject = ['$http', '$q', 'ProductsFactory'];
 
-        constructor (private $http: ng.IHttpService, private $q: ng.IQService) {}
+        constructor (private $http: ng.IHttpService, private $q: ng.IQService, private ProductsFactory: ProductsFactory) {}
 
         getProducts(): ng.IPromise<Product[]> {
             var deferred = this.$q.defer<Product[]>();
-            this.$http.get<any[]>('/products.json').then((response) => {
+            this.$http.get<IProduct[]>('/products.json').then((response) => {
                 var products = response.data;
-                var result: Product[] = [];
-
-                for (let i = 0, len = products.length; i < len; i++) {
-                    let product = products[i];
-                    result.push(new Product(product.name, product.price, product.description));
-                }
+                var result = this.ProductsFactory.create(products);
 
                 deferred.resolve(result);
             });
--- test/services/products_test.ts
+++ test/services/products_test.ts
@@ -4,56 +4,44 @@
 
 describe('ProductsService', () => {
     var ProductsService: MyShop.ProductsService,
-        $httpBackend: ng.IHttpBackendService;
+        $httpBackend: ng.IHttpBackendService,
+        productsResult,
+        ProductsFactory: MyShop.ProductsFactory;
 
     beforeEach(angular.mock.module('MyShop'));
+    beforeEach(angular.mock.module([
+        '$provide',
+        ($provide: ng.auto.IProvideService) => {
+            productsResult = null;
+            $provide.value('ProductsFactory', {
+                create: jasmine.createSpy('ProductsFactory.create').and.callFake(() => {
+                    return productsResult;
+                })
+            });
+        }
+    ]));
     beforeEach(inject([
         'ProductsService',
         '$httpBackend',
-        (p, $hb) => {
+        'ProductsFactory',
+        (p, $hb, pf) => {
             ProductsService = p;
             $httpBackend = $hb;
+            ProductsFactory = pf;
         }
     ]));
 
     it('should fetch products from API', (done) => {
-        var products = [
-            {
-                name: "jPhone 8",
-                price:  999.99,
-                description: "A super modern smartphone!"
-            },
-            {
-                name: "GL 610",
-                price: 1119.99,
-                description: "UltraHD TV with super awesome remote controller made of glass!"
-            },
-            {
-                name: "Venolo G510",
-                price: 345.99,
-                description: "New Venolo's product, super thin and super fast laptop!"
-            }
-        ];
+        var products = ['some', 'array', 'of','products'];
+        productsResult = ['result', 'array', 'of', 'products'];
 
         $httpBackend.expectGET('/products.json').respond(200, products);
 
         ProductsService
             .getProducts()
-            .then((products: MyShop.Product[]) => {
-                expect(products.length).toEqual(3);
-
-                expect(products[0].name).toEqual("jPhone 8");
-                expect(products[0].price).toEqual(999.99);
-                expect(products[0].description).toEqual("A super modern smartphone!");
-
-                expect(products[1].name).toEqual("GL 610");
-                expect(products[1].price).toEqual(1119.99);
-                expect(products[1].description).toEqual("UltraHD TV with super awesome remote controller made of glass!");
-
-                expect(products[2].name).toEqual("Venolo G510");
-                expect(products[2].price).toEqual(345.99);
-                expect(products[2].description).toEqual("New Venolo's product, super thin and super fast laptop!");
-
+            .then((result: MyShop.Product[]) => {
+                expect(ProductsFactory.create).toHaveBeenCalledWith(products);
+                expect(result).toEqual(productsResult);
                 done();
             })
             .catch((err) => {
